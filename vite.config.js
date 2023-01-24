aaed6f81b5cd9fdca702f2594bc9e5e922934ffd
Maria <achigarpia@gmail>
2023-01-24 14:52:12 -0300

    add deploy workflow

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+/*saco la base
+root:"src"
+outDir:"../dist"
+*/
+export default defineConfig({
+  base: "/dhs-laberinto-js/",
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        pages: resolve(root, "pages", "act1", "act1.html"),
+      },
+    },
+  },
+});
