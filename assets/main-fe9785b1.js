4e4d23c4f0110e069a70dc6b797fbdb6ce21b8c5
PiaAchigar <PiaAchigar@users.noreply.github.com>
2023-01-24 18:15:21 +0000

    deploy: 64cf14c8b8687f3a507f74c28ec1cea3b02f31ca

---
+++ assets/main-fe9785b1.js
@@ -0,0 +1,5 @@
+import"./modulepreload-polyfill-ec808ebb.js";document.querySelector("#app").innerHTML=`
+  <div>
+    <h1>Hello from Vite!</h1>
+  </div>
+`;
---
+++ assets/pages-cce3af1e.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-ec808ebb.js";const o="/dhs-laberinto-js/assets/logodhs-a2154629.png";console.log("Actividad 1");document.querySelector("#ancla").innerHTML=`
+<img src="${o}" style="background:black">`;
