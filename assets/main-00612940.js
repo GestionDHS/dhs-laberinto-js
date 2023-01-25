fc4358b9498302fee4078ad83cf3d7b9c84993ca
LeandroMatiasG <LeandroMatiasG@users.noreply.github.com>
2023-01-25 02:12:44 +0000

    deploy: 5a9c0c16a25d76bf548d4bb96c14c313c9abbf57

--- assets/main-00612940.js
+++
@@ -1,5 +0,0 @@
-import"./modulepreload-polyfill-ec808ebb.js";document.querySelector("#app").innerHTML=`
-  <div>
-    <h1>Hello a todos!</h1>
-  </div>
-`;
---
+++ assets/main-c81dc56d.js
@@ -0,0 +1,9 @@
+import"./modulepreload-polyfill-ec808ebb.js";document.querySelector("#app").innerHTML=`
+  <div>
+  <a href="https://www.digitalhouse.com/ar/productos/escuelas">
+  <h1>Actividades Especiales</h1>
+  <img src="../logodhs.png">
+  <p>By <span>Digital House Schools</span></p>
+</a>
+  </div>
+`;
