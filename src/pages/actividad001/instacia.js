78e07377a056cc51bc324ae74e1e5bf765ea5100
Leandro Matías Gómez <lmgomez@DH.digitalhouse.com>
2023-01-25 14:46:59 -0300

    agrego html estatico de referencia

--- src/pages/actividad001/instacia.js
+++ src/pages/actividad001/instacia.js
@@ -1,7 +1,44 @@
-import dh from "../../logodhs.png";
-document.querySelector(".dhs-body").innerHTML = `
-    <section class="panel-izquierdo"></section>
-    <section class="panel-central"></section>
-    <section class="panel-derecho"></section>
+
+document.querySelector("body").innerHTML = `
+
+<!-- seccion izquierda -->
+    <section class="panel-izquierdo">
+     <div id="lista-bloques-disponibles">
+       <h4>Bloques Disponibles</h4>
+       <ul  id="dhs-lista-bloques-disponibles">
+         <!-- ver que onda el script -->
+         <li><div class="bloques-fruta"></div></li>
+         <li><div class="bloques-fruta"></div></li>
+         <li><div class="bloques-fruta"></div></li>
+         <li><div class="bloques-fruta"></div></li>
+         <li><div class="bloques-fruta"></div></li>
+         <li><div class="bloques-fruta"></div></li>
+       </ul>
+     </div>
+    </section>
+
+    <!-- seccion central -->
+
+    <section class="panel-central">
+    <div id="dhs-instrucciones">
+        <h4 id="dhs-encabezado-lista2">Instrucciones</h4>
+        <ul  id="dhs-lista-instrucciones" >
+        <!-- ver que onda el script -->
+       
+        </ul>
+    </div>
+    </section>
+
+    <!-- seccion derecha -->
+
+    <section class="panel-derecho">
+    <div id="panelJuego">
+       <h4 id="dhs-encabezado-desafio">Laberinto</h4>
+       <div id="elemento-escenario"></div>
+       <button >
+         <span>EJECUTAR</span>
+       </button>
+     </div>
+    </section>
 `;
 /**<img src="${dh}" style="background:black"> */
