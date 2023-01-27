a7fb799994263f20758a7e82ef5c3bc4b6f7050e
Maria <achigarpia@gmail>
2023-01-27 10:42:52 -0300

    sortable

--- src/clases/ControladorDeBloques.js
+++ src/clases/ControladorDeBloques.js
@@ -1,4 +1,5 @@
 import { GeneradorDeBloque } from "./GeneradorDeBloque";
+import Sortable from "sortablejs";
 
 export class ControladorDeBloques {
   constructor() {
@@ -12,5 +13,27 @@ export class ControladorDeBloques {
       listaBloques.push(bloqueGenerado);
     });
     return listaBloques;
+  }
+  hacerloSortable(elementoUlOrigen, elementoUlDestino) {
+    //let sortableULOrigen = Sortable.create(elemento, options) o new Sortable(elemento, options)
+
+    Sortable.create(elementoUlOrigen, {
+      group: {
+        name: "sortable",
+        pull: "clone",
+        put: false,
+      },
+      sort: false,
+      animation: 500,
+    });
+    Sortable.create(elementoUlDestino, {
+      group: {
+        name: "sortable",
+        pull: "clone",
+      },
+      sort: true,
+      animation: 400,
+      easing: "cubic-bezier(1, 0, 0, 1)",
+    });
   }
 }
--- src/clases/Juego.js
+++ src/clases/Juego.js
@@ -2,23 +2,25 @@ import { ControladorDeBloques } from "./ControladorDeBloques";
 import { VisualizadorDebugger } from "./VisualizadorDebugger";
 
 export class Juego {
-  constructor(
-    listaBloquesAGenerar
-  ) {
+  constructor(listaBloquesAGenerar) {
     this.controlador = new ControladorDeBloques();
     this.vizualizador = new VisualizadorDebugger();
     this.listaBloquesAGenerar = listaBloquesAGenerar;
     this.listaBloquesDisponibles = document.getElementById(
       "dhs-lista-bloques-disponibles"
-    );
+    );//es el ul
     this.listaBloquesInstrucciones =
-      document.getElementById("dhs-instrucciones");
+      document.getElementById("dhs-lista-instrucciones");
   }
 
   renderizarBloquesDisponibles(listaARenderizar, listaAGenerar) {
-    let listaDeObjetos = this.controlador.crearBloques(listaAGenerar);
+    let listaDeObjetos = this.controlador.crearBloques(
+      listaAGenerar
+    );
     listaDeObjetos.forEach((unBloque) =>
       listaARenderizar.appendChild(unBloque)
     );
+    this.controlador.hacerloSortable(this.listaBloquesDisponibles,this.listaBloquesInstrucciones)
   }
+  
 }
