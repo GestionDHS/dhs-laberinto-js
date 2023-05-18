55a62f46c0ad8f64f1413aeda7d1e9acf0626f57
Maria <achigarpia@gmail>
2023-05-18 08:45:34 -0300

    abre el cofre y muestra el cartel

--- src/clases/Modal.js
+++ src/clases/Modal.js
@@ -1,30 +1,29 @@
 import { DHS_Gallery } from "./Dhs-galeria";
 export class Modal {
-  constructor(modalPannelObj, escenario) {
-    this.escenario = escenario;
+  constructor(modalPannelObj, juego) {
+    this.juego = juego;
     this.oculto = modalPannelObj.oculto;
     this.titulo = modalPannelObj.titulo;
     this.galeria = new DHS_Gallery();
     this.imageUrl = this.galeria.obtenerUrlDe(modalPannelObj.imagen);
     this.texto = modalPannelObj.texto;
-
     this.elementoPannel = document.createElement("DIV");
     this.elementoPannel.classList.add("dhs-modal-pannel");
-
     this.titleElement = document.createElement("P");
     this.titleElement.classList.add("dhs-modal-pannel-title");
     this.imageElement = document.createElement("IMG");
     this.imageElement.classList.add("dhs-modal-pannel-image");
+    
     this.mainTextElement = document.createElement("P");
     this.mainTextElement.classList.add("dhs-modal-pannel-main-text");
 
     this.elementoPannel.appendChild(this.titleElement);
     this.elementoPannel.appendChild(this.imageElement);
     this.elementoPannel.appendChild(this.mainTextElement);
-
+    this.initialize();
     this.juego.escenario.elementoHTML.appendChild(this.elementoPannel);
 
-    this.initialize();
+    
   }
 
   initialize() {
