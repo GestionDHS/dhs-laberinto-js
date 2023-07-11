d8ed01bf889647e412c8142eb400fcf81b231ea6
GestionDHS <GestionDHS@users.noreply.github.com>
2023-07-11 15:14:05 +0000

    deploy: 91564ea03d50d7d92ab4a1d0b59f29111c0666c8

--- assets/main-5b297fe7.js
+++ assets/main-5b297fe7.js
@@ -1,4 +1,4 @@
-import"./style-adc72463.js";const s="/dhs-laberinto-js/assets/logodhs-a2154629.png";document.querySelector("#app").innerHTML=`
+import"./style-a84a8ce8.js";const s="/dhs-laberinto-js/assets/logodhs-a2154629.png";document.querySelector("#app").innerHTML=`
   <div>
   <a href="https://www.digitalhouse.com/ar/productos/escuelas">
   <h1>Actividades Especiales</h1>
