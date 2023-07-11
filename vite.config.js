9e8cd06dce9d4852a3ec451c182cf7195f28904b
veronicagdh <veronica@digitalhouse.com>
2023-07-11 15:28:30 -0300

    DHSgaleria actualziado

--- vite.config.js
+++ vite.config.js
@@ -33,6 +33,7 @@ export default defineConfig({
         act16: resolve(root, "pages", "actividad016", "act.html"),
         act17: resolve(root, "pages", "actividad017", "act.html"),
         act18: resolve(root, "pages", "actividad018", "act.html"),
+        act21: resolve(root, "pages", "actividad021", "act.html"),
       },
     },
   },
