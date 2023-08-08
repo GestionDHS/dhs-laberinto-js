52c9d705bdf03fce70a4a3d0e3cea9e92978a4ab
veronicagdh <verogenoni@hotmail.com>
2023-08-08 11:59:42 -0300

    Carpetas 25, 26

---
+++ src/pages/actividad025/instancia.js
@@ -0,0 +1,193 @@
+import { Juego } from "../../clases/Juego";
+import { template } from "../../recursosPaginas/Template";
+import ControladorStandard from "../../bloques/Controlador";
+import { CustomRenderer } from "../../bloques/CustomRender";
+import customTheme from "../../bloques/CustomTheme";
+import { CustomCategory } from "../../bloques/CustomCategory";
+import {Dhs_personajes} from "../../clases/Dhs-personajes"
+import {generarCoordenadas, configurarYRenderizarToolbox} from '../../Utils/Funciones';
+import {Dhs_Categorias} from '../../clases/Dhs-categorias';
+
+document.querySelector("#appActividad").innerHTML = template(``);
+const velocidadInicial = 1000;
+const miJuego = new Juego(velocidadInicial);
+const dimensiones = [3, 7]; //fila, columna
+
+const tablero = [
+  [1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1],
+];
+let coordenadasCaminoPared = generarCoordenadas(tablero);
+const galeriaPersonajes = new Dhs_personajes();
+const pared = galeriaPersonajes.obtenerPersonaje("arbol")
+const camino = galeriaPersonajes.obtenerPersonaje("pasto")
+
+const datosModal = {
+  titulo: "¡BUEN TRABAJO!",
+  imagen: "monedas",
+  texto: "Encontramos 180 monedas de oro.",
+  oculto: true,
+};
+miJuego.generarEscenario(dimensiones, 3, "white");
+miJuego.agregarModal(datosModal);
+
+const lupe = galeriaPersonajes.obtenerPersonaje("lupe")
+const cofre = galeriaPersonajes.obtenerPersonaje("cofre")
+const lodo = galeriaPersonajes.obtenerPersonaje("lodo")
+const basura = galeriaPersonajes.obtenerPersonaje("basura")
+
+const conjuntosDePersonajes = [
+  {
+    estrategia: "fijos",
+    personajes: [pared],
+    posiciones: coordenadasCaminoPared.coordenadasPared,
+    aliasConjunto: "fijosTablero",
+    desapareceAlReiniciar: false,
+  },
+  {
+    estrategia: "fijos",
+    personajes: [camino],
+    posiciones: coordenadasCaminoPared.coordenadasCamino,
+    aliasConjunto: "fijosTablero",
+    desapareceAlReiniciar: false,
+  },
+  {
+    estrategia: "fijos",
+    personajes: [lupe],
+    posiciones: [[3, 3]],
+    aliasConjunto: "fijoPrincipal",
+    desapareceAlReiniciar: false,
+  },
+  {
+    estrategia: "fijos",
+    personajes: [lodo],
+    posiciones: [[1, 3]],
+    aliasConjunto: "fijoLodo",
+    desapareceAlReiniciar: false,
+  },
+  {
+    estrategia: "fijos",
+    personajes: [cofre],
+    posiciones: [[3, 4]],
+    aliasConjunto: "fijoCofre",
+    desapareceAlReiniciar: false,
+  },
+  {
+    estrategia: "fijos",
+    personajes: [basura],
+    posiciones: [[2, 2]],
+    aliasConjunto: "fijoBasura",
+    desapareceAlReiniciar: false,
+  },
+]
+miJuego.crearPersonajes(conjuntosDePersonajes);
+miJuego.setearPersonajePrincipal(miJuego.listaDePersonajes[30]);
+
+//Método para Abrir el Cofre
+// miJuego.personajePrincipal.abrirCofre = function () {
+//   const intento = this.buscarParaRealizarAccion("cofre", "abrirse");
+
+//   if (!intento.objetoEncontrado) {
+//     return this.decirTerminar("Oh! Aquí no hay cofre.");
+//   } else if (!intento.exito) {
+//     return this.decirTerminar("Oh! Este cofre ya estaba abierto.");
+//   } else {
+//     return this.abrirYMostrarModal();
+//   }
+// };
+
+// //Método para Juntar Basura
+// miJuego.personajePrincipal.juntarBasura = function () {
+//   const intento = this.buscarParaRealizarAccion("basura", "serJuntado");
+//   if (!intento.objetoEncontrado) {
+//     this.decirTerminar("Oh! Aquí no hay basura.");
+//   } else if (!intento.exito) {
+//     this.decirTerminar("Oh! Hubo un problema al juntar la basura.");
+//   }
+//   return intento;
+// };
+
+// // Lancha
+// miJuego.personajePrincipal.llegarPlanta = function () {
+//   if (this.mochila.length === 3) {
+//     this.abrirYMostrarModal();
+//   } else if(!this.intento) {
+//     return this.decirTerminar("¡Oh! Quedó basura por levantar.")
+//   }
+// }
+
+// // Pedro - Lupe
+// miJuego.personajePrincipal.llegarEscuela = function () {
+//   this.abrirYMostrarModal();
+// }
+
+// //Conejo - Nelson
+// miJuego.personajePrincipal.cosecharZanahoria = function () {
+//   const intento = this.buscarParaRealizarAccion("zanahoria", "abrirse");
+//   if (!intento.objetoEncontrado) {
+//     return this.decirTerminar("¡Oh! Aquí no hay zanahoria.");
+//   } else if (!intento.exito) {
+//     return this.decirTerminar("¡Oh! Esta zanahoria ya fue cosechada.");
+//   } 
+// };
+
+// miJuego.personajePrincipal.comerZanahoria = function () {
+//   const intento = this.buscarParaRealizarAccion("zanahoria", "serJuntado");
+ 
+//   if (!intento.objetoEncontrado) {
+//     return this.decirTerminar("¡Oh! Aquí no hay zanahoria.");
+//   } else if( intento.estado == "juntado") {
+//     return this.decirTerminar("¡Oh! Esta zanahoria ya fue comida.");
+//   } else if (!intento.exito ) {
+//     return this.decirTerminar("¡Oh! Esta zanahoria aún no fue cosechada.");
+//   } 
+// };
+
+//Seteo del Dibujo a realizar - Verificación. Para los ejercicios que tienen PersonajesDibujables
+// const EJEMPLO_DIBUJO_DESEADO = [
+//   [null, null, null, null, null],
+//   [null, "#000000", null, "#000000", null],
+//   [null, "#000000", null, "#000000", null],
+//   [null, null, null, null, null],
+// ]
+// miJuego.personajePrincipal.dibujoDeseado = EJEMPLO_DIBUJO_DESEADO
+
+//******************************************************* */
+//    BLOCKLY
+//****************************************************** */
+
+//OCTAVO: Creamos una instancia del controlador, argumentos: el juego, velocidad inicial
+
+//****Si necesitamos que el Workspace tenga bloques precargados, lo que hacemos, para obtener el JSON para setearlo es
+//****lo siguiente: 
+//****1- hacemos global a miControlador "window.miControlador"
+//****2 - En el Navegador, cargamos los bloques que necesitamos ya estén preCargados
+//****3 - Ponemos en consola éste linea: JSON.stringify(miControlador.obtenerBloquesSerializados()) para Obtener bloques precargados
+//****4 - Nos copiamos el string que nos devuelve, y se lo colocamos a la variable "bloquesPrecargadosJSON"
+//****5 - Volvemos a poner "miControlador" como const
+
+// BLOCKLY ------------------------------------------------------
+const miControlador = new ControladorStandard(miJuego,velocidadInicial);
+const categoria=new Dhs_Categorias()
+const categoriaElegida=categoria.obtenerCategoria("lapizARCondicional")
+
+const ordenJerarquicoBloques = [
+  ["on_execute", "Eventos"],
+  ["move_classic_simple", "Movimientos"],
+  ["move_classic_param", "Movimientos"],
+  ["avanzar_param", "Movimientos"],
+  ["girar_clasico", "Movimientos"],
+  ["girar_grados", "Movimientos"],
+  ["apuntar_hacia", "Movimientos"],
+  ["abrir_cofre", "Acciones"],
+  ["juntar_basura", "Acciones"],
+  ["lapiz", "Lápiz"],
+  ["if", "Condicionales"],
+  ["controls", "Repeticiones"],
+];
+const bloquesPrecargadosJSON ='{"blocks":{"languageVersion":0,"blocks":[{"type":"on_execute","id":"rwW]g?!-iwJNk))r*~^C","x":61,"y":69}]}}';
+//const bloquesPrecargadosJSON ='{"blocks":{"languageVersion":0,"blocks":[{"type":"on_execute","id":"rwW]g?!-iwJNk))r*~^C","x":61,"y":69,"inputs":{"EVENT":{"block":{"type":"avanzar_param","id":"=#y0[*$GJ+W{WlW|MSqI","fields":{"CASILLAS":1},"next":{"block":{"type":"girar_derecha","id":"^*0eVn,V}s/U%UV3z|d;"}}}}}}]}}'
+const funcionesAExponer=["juntarBasura","avanzar","abrirCofre"]
+
+configurarYRenderizarToolbox(miControlador,categoriaElegida,ordenJerarquicoBloques,bloquesPrecargadosJSON,funcionesAExponer)
