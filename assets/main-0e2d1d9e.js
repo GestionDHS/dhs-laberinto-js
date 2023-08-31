c6e4340c8672c577c31081178e978cc820103027
GestionDHS <GestionDHS@users.noreply.github.com>
2023-08-31 18:48:58 +0000

    deploy: 66b54cea573a64639ede819169c5927423fa4d68

--- assets/main-0e2d1d9e.js
+++
@@ -1,9 +0,0 @@
-import"./style-89e97fe3.js";const s="/dhs-laberinto-js/assets/logodhs-a2154629.png";document.querySelector("#app").innerHTML=`
-  <div>
-  <a href="https://www.digitalhouse.com/ar/productos/escuelas">
-  <h1>Actividades Especiales</h1>
-  <img src="${s}">
-  <p>By <span>Digital House Schools</span></p>
-</a>
-  </div>
-`;
