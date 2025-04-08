b02c9a9b1ef4ee439b52131ff0194cf358f2a657
Mariano Martin <mariano.martin@digitalhouse.com>
2025-04-08 17:19:58 -0300

    Updates

---
+++ assets/main-6e271ecb.js
@@ -0,0 +1,9 @@
+import"./style-fd100aa9.js";const s="/dhs-laberinto-js/assets/logodhs-a2154629.png";document.querySelector("#app").innerHTML=`
+  <div>
+  <a href="https://www.digitalhouse.com/ar/productos/escuelas">
+  <h1>Actividades Especiales</h1>
+  <img src="${s}">
+  <p>By <span>Digital House Schools</span></p>
+</a>
+  </div>
+`;
